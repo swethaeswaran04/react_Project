ade4ca65987ac96b9ea91027d4a77468579002c3
swethaeswaran04 <150774265+swethaeswaran04@users.noreply.github.com>
2024-10-28 14:49:29 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,20 @@
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './Home';
+import Login from './Login';
+import SignUp from './SignUp';
+import MainPage from './MainPage';
+
+const App = () => (
+  <Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/main" element={<MainPage />} />
+    </Routes>
+  </Router>
+);
+
+export default App;
